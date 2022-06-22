240f2f11240651ef7c30603bdb8ad9c15443b556
horlorlahdeh <33106252+horlorlahdeh@users.noreply.github.com>
2022-06-22 10:24:26 +0100

    get speakers

--- src/pages/Speakers.jsx
+++ src/pages/Speakers.jsx
@@ -1,20 +1,28 @@
-import React from "react";
-import Navbar from "../components/Navbar";
-import Register from "../components/Register";
-import Date from "../components/speakers/Date";
-import Developers from "../components/speakers/Developers";
-import Hero from "../components/speakers/Hero";
-import SignUp from "../components/speakers/SignUp";
+import React, { useState, useEffect } from 'react';
+import Navbar from '../components/Navbar';
+import Register from '../components/Register';
+import Date from '../components/speakers/Date';
+import Developers from '../components/speakers/Developers';
+import Hero from '../components/speakers/Hero';
+import SignUp from '../components/speakers/SignUp';
+import axiosInstance from '../utils/axios';
 
 const Speakers = () => {
+  const [speakers, setSpeakers] = useState([]);
+  useEffect(() => {
+    axiosInstance.get('/speakers').then((res) => {
+      setSpeakers(res.data.data);
+      console.log(res.data.data);
+    });
+  }, [setSpeakers]);
   return (
     <div>
-      <Navbar title="virtual conference speakers" />
+      <Navbar title='virtual conference speakers' />
       <Hero />
-      <Developers/>
-      <SignUp/> 
-      <Date/> 
-      <Register/>
+      <Developers speakers={speakers} />
+      <SignUp />
+      <Date />
+      <Register />
     </div>
   );
 };
